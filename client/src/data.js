471b6e19d1ae1033061b50ac4e55a55b54910388
Tien1411 <nguyenviettien1411@gmail.com>
2024-03-03 22:19:58 +0700

    first commit

---
+++ client/src/data.js
@@ -0,0 +1,38 @@
+import Thumbnail1 from "./images/blog1.jpg";
+import Thumbnail2 from "./images/blog2.jpg";
+import Thumbnail3 from "./images/blog3.jpg";
+import Thumbnail4 from "./images/blog4.jpg";
+export const DUMMY_POST = [
+    {
+      id: "1",
+      thumbnail: Thumbnail1,
+      category: "education",
+      title: "This is the title of the very first post on this blog",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      authorID: 3,
+    },
+    {
+      id: "2",
+      thumbnail: Thumbnail2,
+      category: "education",
+      title: "This is the title of the very first post on this blog",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      authorID: 1,
+    },
+    {
+      id: "3",
+      thumbnail: Thumbnail3,
+      category: "education",
+      title: "This is the title of the very first post on this blog",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      authorID: 2,
+    },
+    {
+      id: "4",
+      thumbnail: Thumbnail4,
+      category: "education",
+      title: "This is the title of the very first post on this blog",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      authorID: 4,
+    },
+  ];
